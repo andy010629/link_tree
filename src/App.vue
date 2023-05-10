6ed89eb914c8bc5365222f44b9175c9056931e67
andy010629 <andy010629@gmail.com>
2023-05-10 12:13:38 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import LinkButton from './components/LinkButton.vue'
+
+
+const link_data = [
+   { title:'個人網站', url:'https://nulong.me'},
+
+  { title:'好保 - 投資型保單整合平台', url:'https://haobao.nulong.me'},
+   { title:'布林科技官網', url:'https://bullinv.tech'},
+   { title:'外匯策略管理後台', url:'https://fx.bullinv.tech'},
+  { title:'台主記錄器', url:'https://claw.nulong.me'},
+  { title:'演算法作業', url:'https://algohw.nulong.me'},
+  { title:'國家與市場 - 主持人投影片', url:'https://docs.google.com/presentation/d/103EdDxqWLoLa2F6ocIBfpbboU1zC9fEnAT3-yeF-Mwc/edit?usp=sharing'}
+]
+
+</script>
+
+<template>
+  <div>
+    <div class="w-full flex justify-center text-4xl font-bold text-gray-200 tracking-wider">個人連結 mylink</div>
+    <div class="w-full mt-12 grid  grid-cols-1 md:grid-cols-3 gap-6 md:gap-12">
+      <LinkButton v-for="link in link_data" :key="link.title" :title="link.title" :url="link.url" />
+    </div>
+    <div class="h-24"></div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/LinkButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div @click="link_to(props.url)" style="cursor: pointer;" class="hover:opacity-90 hover:-translate-y-2 duration-300 p-6 px-4  text-xl font-bold  bg-slate-300 shadow rounded-lg shadow-slate-100">
+    <div class="text-gray-800 select-none hover:text-gray-700 w-full"  :href="props.url" target="_blank">{{ props.title }}</div>
+</div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+const props = defineProps({
+  title: String,
+  url: String,
+})
+
+function link_to(url: string) {
+  window.open(url, '_blank')
+}
+
+</script>
+
+<style scoped>
+
+</style>
