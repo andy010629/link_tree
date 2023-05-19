51ec69fe4005e013ed4b2b662ad4bd4cbe2d38f6
andy010629 <andy010629@gmail.com>
2023-05-19 10:40:26 +0800

    add link

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,13 @@ import LinkButton from './components/LinkButton.vue'
 
 const link_data = [
    { title:'個人網站', url:'https://nulong.me'},
-
   { title:'好保 - 投資型保單整合平台', url:'https://haobao.nulong.me'},
-   { title:'布林科技官網', url:'https://bullinv.tech'},
-   { title:'外匯策略管理後台', url:'https://fx.bullinv.tech'},
+  { title:'好保介紹投影片', url:'https://haobao.nulong.me'},
+  { title:'布林科技', url:'https://bullinv.tech'},
+  { title:'外匯策略管理後台', url:'https://fx.bullinv.tech'},
   { title:'台主記錄器', url:'https://claw.nulong.me'},
-  { title:'演算法作業', url:'https://algohw.nulong.me'},
-  { title:'國家與市場 - 主持人投影片', url:'https://docs.google.com/presentation/d/103EdDxqWLoLa2F6ocIBfpbboU1zC9fEnAT3-yeF-Mwc/edit?usp=sharing'}
+  { title:'演算法實作', url:'https://algohw.nulong.me'},
+  // { title:'國家與市場 - 主持人投影片', url:'https://docs.google.com/presentation/d/103EdDxqWLoLa2F6ocIBfpbboU1zC9fEnAT3-yeF-Mwc/edit?usp=sharing'}
 ]
 
 </script>
